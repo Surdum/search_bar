93a2054e0c731ee21318dc0b3bc13f533f767a04
Satan <31803702+Surdum@users.noreply.github.com>
2018-06-01 23:55:35 +0300

    r

--- search_bar/search_bar.py
+++ search_bar/search_bar.py
@@ -7,4 +7,4 @@ bars = json.loads(getBarsDB())
 def getNearestBar(userLocation):
     bar, dist = calculations.getNearestBar(userLocation, bars)
     return [bar['Name'], bar['Address'], bar['PublicPhone'][0]['PublicPhone'],
-            bar['Latitude_WGS84'], bar['Longitude_WGS84'], int(dist * 1500) + 1]
+            bar['Latitude_WGS84'], bar['Longitude_WGS84']]
